0a4319d7c3b90835c75f73d7ca7c4f19ae275767
dzikr <dzikrikovalesky12@gmail.com>
2022-10-18 09:07:10 +0700

    RuntimeError: Working outside of application context. 2

--- db_create.py
+++ db_create.py
@@ -7,6 +7,7 @@ recipe1 = Recipe('Slow-Cooker Tacos', 'Delicious ground beef that has been simme
                                       'Perfect with hard-shelled tortillas!')
 recipe2 = Recipe('Hamburgers', 'Classic dish elevated with pretzel buns.')
 recipe3 = Recipe('Mediterranean Chicken', 'Grilled chicken served with pitas, hummus, and sauted vegetables.')
+
 db.session.add(recipe1)
 db.session.add(recipe2)
 db.session.add(recipe3)
--- project/models.py
+++ project/models.py
@@ -3,15 +3,14 @@ from project import db
 
 class Recipe(db.Model):
     __tablename__ = "recipes"
-
     id = db.Column(db.Integer, primary_key=True)
     recipe_title = db.Column(db.String, nullable=False)
     recipe_description = db.Column(db.String, nullable=False)
 
-    def __int__(self, title, description):
+    def __init__(self, title, description):
         self.recipe_title = title
         self.recipe_description = description
 
-    def __repr__(self):
-        return '<title {}', format(self.name)
 
+def __repr__(self):
+    return '<title {}'.format(self.name)
